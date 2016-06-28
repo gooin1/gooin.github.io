7f8ccd8d2e0b9c535fd503828fba9e3dc3e8e06e
gooin <gooin@outlook.com>
2016-06-28 20:33:56 +0800

    使用js实现显示最后编辑页面的时间

---
+++ js/showLastModified.js
@@ -0,0 +1,9 @@
+/**
+ * Created by gooin on 2016/6/28.
+ */
+var msg = '<p><b>最后编辑:</b>'+ document.lastModified +'</p>';
+
+var el = document.getElementById('last_modified');
+el.innerHTML=msg; 
+
+
